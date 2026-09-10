5500ffa77c3ba8c127f37dcaaa0d79c95705a3cd
wickidcow <jaywago@gmail.com>
2026-09-10 11:39:04 -0400

    compat: update health and nausea API names

--- src/main/java/io/github/sefiraat/slimetinker/events/friend/EventChannels.java
+++ src/main/java/io/github/sefiraat/slimetinker/events/friend/EventChannels.java
@@ -36,7 +36,7 @@ public final class EventChannels {
             int amplifier = entry.getValue() - 1;
             friend.getPlayer().addPotionEffect(new PotionEffect(potionEffectType, tickDuration, amplifier, false, false, true));
             if (resetHealth) {
-                double maxHealth = p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
+                double maxHealth = p.getAttribute(Attribute.MAX_HEALTH).getValue();
                 if (health > maxHealth) {
                     health = maxHealth;
                 }
@@ -53,7 +53,7 @@ public final class EventChannels {
      * @return int for number of bonus ticks required
      */
     private static int getBonusTicks(PotionEffectType potionEffectType) {
-        if (potionEffectType.equals(PotionEffectType.CONFUSION)) {
+        if (potionEffectType.equals(PotionEffectType.NAUSEA)) {
             return 70;
         } else if (potionEffectType.equals(PotionEffectType.NIGHT_VISION)) {
             return 210;
